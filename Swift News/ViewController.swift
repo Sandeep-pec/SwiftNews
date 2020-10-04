78febf6afb96ac1241801e6b90dcadf5bb60dff0
Sandeep Sangwan <sandeepsangwan.pec@gmail.com>
2020-10-03 20:37:49 -0700

    Initial Commit

---
+++ Swift News/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift News
+//
+//  Created by Sandeep Sangwan on 2020-10-03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
